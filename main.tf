40fa6e06f41676d983d9d190736e163e4a291170
emvaugh2 <edwardvaughnv@outlook.com>
2025-02-28 14:02:12 -0600

    Hashicorp website sent me off.

--- main.tf
+++ main.tf
@@ -181,7 +181,9 @@ resource "azurerm_lb_rule" "lbrule" {
 # Creates Azure Bastion host
 
 resource "azurerm_bastion_host" "bastion" {
-    namme = myBastionHost
+    resource_group_name = var.rg
+    location = var.loc
+    name = "myBastionHost"
     subnet_id = azurerm_subnet.bastionsubnet.id
     public_ip_address_id = azurerm_public_ip.bpip.id
 
