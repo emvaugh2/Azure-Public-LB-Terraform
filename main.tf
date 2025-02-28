c02c22ac6c88453a5a4c5bb13ccbae122e35d07c
emvaugh2 <edwardvaughnv@outlook.com>
2025-02-28 11:06:35 -0600

    Filled in more of the Terraform code

--- main.tf
+++ main.tf
@@ -1,5 +1,100 @@
+
+# Creates the Resource Group for our entire project
 resource "azurerm_resource_group" "lb_rg" {
     name = var.rg # name of the resource azurerm_resource_group
     location = var.loc # Azure region where it will be deployed
 }
 
+# Creates the Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+    name = var.vnet
+    location = var.loc
+    resource_group_name = var.rg
+    address_space = ["10.0.0.0/16"]
+
+}
+    
+# Creates the Backend pool subnet
+resource "azurerm_subnet" "backsubnet" {
+    name = var.backsubnet
+    resource_group_name = var.rg
+    virtual_network_name = var.vnet
+    address_prefixes = ["10.1.0.0/24"]
+}
+
+# Creates the Azure Bastion subnet
+resource "azurerm_subnet" "bastionsubnet" {
+    name = var.bastionsubnet
+    resource_group_name = var.rg
+    virtual_network_name = var.vnet
+    address_prefixes = ["10.1.1.0/27"]
+}
+
+
+
+# Creates the Public IPs for our resources
+
+resource "azurerm_public_ip" "lbpip" {
+    name = var.lbpip
+    resource_group_name = var.rg
+    location = var.loc
+    allocation_method = "Static"
+}
+
+resource "azurerm_public_ip" "bpip" {
+    name = var.bpip
+    resource_group_name = var.rg
+    location = var.loc
+    allocation_method = "Static"
+}
+
+resource "azurerm_public_ip" "natpip" {
+    name = var.natpip
+    resource_group_name = var.rg
+    location = var.loc
+    allocation_method = "Static"
+}
+
+# Creates the Network Security Group
+
+resource "azurerm_network_security_group" "nsg" {
+    name = var.nsg
+    location = var.loc
+    resource_group_name = var.rg
+
+    security_rule {
+        name = "myNSGRuleHTTP"
+        priority = 200
+        direction = "inbound"
+        access = "Allow"
+        protocol = "*"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_network_interface" "nic1" {
+    name = var.nic1
+    location = var.loc
+    resource_group_name = var.rg
+
+    ip_configuration {
+        name = "ipconfig1"
+        subnet_id = azurerm_subnet.backsubnet.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_network_interface" "nic2" {
+    name = var.nic2
+    location = var.loc
+    resource_group_name = var.rg
+
+    ip_configuration {
+        name = "ipconfig1"
+        subnet_id = azurerm_subnet.backsubnet.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,122 @@
+
+# Resource Group variable 
 variable "rg" {
     description = "The name of the Azure Resource Group"
     type = string
     default = "CreatePubLBQStest-rg"
 }
 
+# Location variable
 variable "loc" {
     description = "The location where we're deploying all of our resources"
     type = string
     default = "East US"
 }
 
+# Virtual Network variable
+variable "vnet" {
+    description = "The name of the Virtual Network for our project."
+    type = string
+    default = "myVNet"
+}
+
+# Backend Pool Subnet variable
+variable "backsubnet" {
+    description = "The name of the subnet for out Backend pool."
+    type =  string
+    default = "myBackendSubnet"
+}
+
+# Azure Bastion Subnet variable
+variable "bastionsubnet" {
+    description = "The name of the subnet for out Bastion resource."
+    type =  string
+    default = "AzureBastionSubnet"
+}
+
+# Load Balancer Public IP name variable
+variable "lbpip" {
+    description = "The name of the load balancer's public IP address"
+    type = string
+    default = "myPublicIP"
+}
+
+# Azure Bastion host Public IP name variable
+variable "bpip" {
+    description = "The name of the Azure Bastion's public IP address"
+    type = string
+    default = "myBastionIP"
+}
+
+# NAT Gateway Public IP name variable
+variable "natpip" {
+    description = "The name of the NAT Gateway's public IP address"
+    type = string
+    default = "myNATgatewayIP"
+}
+
+
+# Load Balancer name variable
+variable "lb" {
+    description = "The name of the load balancer"
+    type = string
+    default = "myLoadBalancer"
+}
+
+# Load Balancer Health Probe name variable
+variable "lbhp" {
+    description = "The name of the load balancer's health probe"
+    type = string
+    default = "myHealthProbe"
+}
+
+# Network Security Group name variable
+variable "nsg" {
+    description = "The name of the network security group"
+    type = string
+    default = "myNSG
+}
+
+# Azure Bastion host name variable
+variable "bastion" {
+    description = "The name of the Azure Bastion host resource"
+    type = string
+    default = "myBastionHost"
+}
+
+################################ Finish the stuff below
+
+# Network Interface 1 name variable
+variable "nic1" {
+    description = "The name of virtual machine 1's NIC"
+    type = string
+    default = "myNicVM1"
+}
+
+# Network Interface 2 name variable
+variable "nic2" {
+    description = "The name of virtual machine 2's NIC"
+    type = string
+    default = "myNicVM2"
+}
+
+# Virtual Machine 1 name variable
+variable "vm1" {
+    description = "The name of virtual machine 1"
+    type = string
+    default = "myVM1"
+}
+
+# Virtual Machine 2 name variable
+variable "vm2" {
+    description = "The name of virtual machine 2"
+    type = string
+    default = "myVM2"
+}
+
+# NAT Gateway name variable
+variable "nat" {
+    description = "The name of the NAT gateway resource"
+    type = string
+    default = "myNATgateway"
+}
